d998dccce139dcf999e9fdbc3123ce6a46b4f5b3
curtis clemmons <curtisclemmons2000@icloud.com>
2020-09-06 17:30:49 -0400

    Initial Commit

---
+++ project/src/Projectile.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+
+export var strength : float = 500
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_up"):
+		var direction = Vector2(1,-1).normalized()
+		var velocity = direction * strength
+		apply_impulse(Vector2.ZERO, velocity)
