fa05f45537fee094efb501f545e1fe5a327ae744
curtis clemmons <curtisclemmons2000@icloud.com>
2020-09-13 14:59:07 -0400

    update projectile script to show angle which projectile will fire

--- project/src/Projectile.gd
+++ project/src/Projectile.gd
@@ -4,19 +4,25 @@ export var strength : float = 500
 var ready_for_launch : bool = true
 var in_the_air : bool = false
 var projectile_state : bool = ready_for_launch 
-var direction = Vector2(1,-1).normalized()
+var _angle : float = 0
 
-func _process(delta):
+func _process(_delta):
 	if projectile_state == ready_for_launch:
 		if Input.is_action_just_pressed("RotateAngleLeft"):
-			direction = Vector2(0.5,-1)
+			_angle -= PI / 16
 		if Input.is_action_just_pressed("RotateAngleRight"):
-			direction = Vector2(1.5,-1)
+			_angle += PI / 16
 		if Input.is_action_just_pressed("Launch"):
+			var direction = Vector2(1,0).rotated(_angle)
 			var velocity = direction * strength
 			apply_impulse(Vector2.ZERO, velocity)
 			projectile_state  = in_the_air
+			
+	update()
 	if Input.is_action_just_pressed("IncreaseStrength"):
 		strength += 25
 	if Input.is_action_just_pressed("DecreaseStrength"):
 		strength -= 25
+		
+func _draw():
+	draw_line(Vector2.ZERO, Vector2(80, 0).rotated(_angle), Color.aliceblue, 5)
