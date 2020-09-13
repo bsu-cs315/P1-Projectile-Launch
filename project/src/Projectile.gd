ea21795e659f72f0b4f8fd19a5bef77370dbc923
curtis clemmons <curtisclemmons2000@icloud.com>
2020-09-13 15:28:09 -0400

    hide the angle after projectile is launched

--- project/src/Projectile.gd
+++ project/src/Projectile.gd
@@ -1,24 +1,24 @@
 extends RigidBody2D
 
 export var strength : float = 500
-var ready_for_launch : bool = true
-var in_the_air : bool = false
-var projectile_state : bool = ready_for_launch 
+export var rotation_speed : float = 1
+
+var _launched := false
 var _angle : float = 0
 
-func _process(_delta):
-	if projectile_state == ready_for_launch:
-		if Input.is_action_just_pressed("RotateAngleLeft"):
-			_angle -= PI / 16
-		if Input.is_action_just_pressed("RotateAngleRight"):
-			_angle += PI / 16
-		_angle = clamp(_angle, - PI /2, 0)
-		if Input.is_action_just_pressed("Launch"):
-			var direction = Vector2(1,0).rotated(_angle)
-			var velocity = direction * strength
-			apply_impulse(Vector2.ZERO, velocity)
-			projectile_state  = in_the_air
-			
+func _process(delta):
+	if _launched:
+		return
+	if Input.is_action_pressed("RotateAngleLeft"):
+		_angle -= rotation_speed * delta
+	if Input.is_action_pressed("RotateAngleRight"):
+		_angle += rotation_speed * delta
+	_angle = clamp(_angle, - PI /2, 0)
+	if Input.is_action_pressed("Launch"):
+		var direction = Vector2(1,0).rotated(_angle)
+		var velocity = direction * strength
+		apply_impulse(Vector2.ZERO, velocity)
+		_launched = true
 	update()
 	if Input.is_action_just_pressed("IncreaseStrength"):
 		strength += 25
@@ -26,4 +26,5 @@ func _process(_delta):
 		strength -= 25
 		
 func _draw():
-	draw_line(Vector2.ZERO, Vector2(80, 0).rotated(_angle), Color.aliceblue, 5)
+	if not _launched:
+		draw_line(Vector2.ZERO, Vector2(80, 0).rotated(_angle), Color.aliceblue, 5)
