99b513aad2b1bb7964336c0f32fd2a02dc4644ce
curtis clemmons <curtisclemmons2000@icloud.com>
2020-09-20 18:55:26 -0400

    Fix the warnings in the debugger

--- project/src/TitleScreen.gd
+++ project/src/TitleScreen.gd
@@ -7,4 +7,5 @@ func _add_a_scene_manually():
 	get_tree().get_root().add_child(simultameous_scene)
 
 func _on_PlayButton_pressed():
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://project/src/Level.tscn")
